5bef24053b9a9deee33616b8d8d3626363dab934
Kyle Potts <kyle.potts1@angieslist.com>
2017-06-06 10:27:56 -0400

    use two refs

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import Dropzone from 'react-dropzone';
 
-let dropzoneRef;
+let dropzoneRef1;
+let dropzoneRef2;
 
 class App extends Component {
 
   render() {
     return (
       <div>
-        <Dropzone ref={(node) => { dropzoneRef = node; }} onDrop={(accepted, rejected) => { alert(accepted) }}>
+        <Dropzone ref={(node) => { dropzoneRef1 = node; }} onDrop={(accepted, rejected) => { alert(accepted) }}>
           <p>Drop files here.</p>
         </Dropzone>
-        <button type="button" onClick={() => { console.log('this will execute twice'); dropzoneRef.open() }}>
+        <button type="button" onClick={() => { console.log('this will execute twice'); dropzoneRef1.open() }}>
           Open File Dialog
       </button>
-      <Dropzone ref={(node) => { dropzoneRef = node; }} onDrop={(accepted, rejected) => { alert(accepted) }}>
+      <Dropzone ref={(node) => { dropzoneRef2 = node; }} onDrop={(accepted, rejected) => { alert(accepted) }}>
         <p>Drop files here.</p>
       </Dropzone>
       <button type="button" onClick={() => { console.log('this will execute once');}}>
